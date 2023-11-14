26b0681104c8225a44d6718344591d5c062d052f
2508roblox <trangiangzxc@gmail.com>
2023-11-14 19:03:03 +0700

    apply_middleware

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require( "express");
-import cors from 'cors'
-var bodyParser = require('body-parser')
-require('dotenv').config();
-const authRouter = require( './src/router/authRoute'); 
-// 
-const port  = process.env.PORT || 4000;
-const app = express()
+const express = require("express");
+import cors from "cors";
+var bodyParser = require("body-parser");
+require("dotenv").config();
+const authRouter = require("./src/router/authRoute");
+import { notFound, errorHandler } from "./src/middleware/errorMiddleware";
+const port = process.env.PORT || 4000;
+const app = express();
 // middleware
-app.use(cors())
-app.use(bodyParser.json()) 
-app.use(bodyParser.urlencoded({ extended: true })) 
-app.use(express.json())
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 // apis
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
 
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log('connect success on '+ port)
-})
+  console.log("connect success on " + port);
+});
--- src/router/authRoute.js
+++ src/router/authRoute.js
@@ -6,6 +6,7 @@ import authController from '../controllers/authController'
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
+router.post("/refresh-token", authController.logout);
 
 // About page route.
 router.get("/about", function (req, res) {
